f6b9690f868d4da8c6d9f8b9d1778164c1ff72b3
Shin'ya Ueoka <ueokande@i-beam.org>
2017-05-13 19:19:08 +0900

    Add tests of getCsrfToken

--- assets/js/shared/csrf.js
+++ assets/js/shared/csrf.js
@@ -2,8 +2,8 @@ function getCsrfToken() {
   var fields = document.cookie.split(';');
   for (var i = 0; i < fields.length; ++i) {
     var kv = fields[i].split('=');
-    if (kv[0] === "_csrf") {
-      return kv[1];
+    if (kv[0].trim() === "_csrf") {
+      return kv[1].trim();
     }
   }
   return null;

ea2ad9eaebeb11f048a5e6973bc4dec821cc8c19
Shin'ya Ueoka <ueokande@i-beam.org>
2017-05-15 21:54:52 +0900

    Clean login

--- assets/js/login/index.jsx
+++ assets/js/login/index.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import LoginForm from './login-form';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <hgroup>
+          <h1>Mugil tasks</h1>
+          <h3>Accelerate your task processing</h3>
+        </hgroup>
+        <LoginForm />
+      </div>
+    );
+  }
+}
 
 window.addEventListener('load', () => {
   var login = document.getElementById('mugil-login');
   if (login !== null) {
     ReactDOM.render(
       <MuiThemeProvider>
-        <div>
-          <hgroup>
-            <h1>Mugil tasks</h1>
-            <h3>Accelerate your task processing</h3>
-          </hgroup>
-          <LoginForm />
-        </div>
+        <Login />
       </MuiThemeProvider>,
       login
     );

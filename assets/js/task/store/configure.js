be193e1521048b855aa34e82db6281aa5eef53b9
Shin'ya Ueoka <ueokande@i-beam.org>
2017-05-20 14:53:28 +0900

    Use redux-thunk

--- assets/js/task/store/configure.js
+++ assets/js/task/store/configure.js
@@ -1,10 +1,12 @@
 import logger  from 'redux-logger';
+import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducer/index';
 
 export default function createStoreWithMiddleware() {
   const store = applyMiddleware(
-    logger
+    logger,
+    thunk
   )(createStore);
   return store(rootReducer);
 }
